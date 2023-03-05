506fda4fc6a6edad2415126df8c3f64f7aef4880
k-pulkit <pulkit42041@gmail.com>
2023-03-05 07:19:23 -0500

    Wrote the docker-compose to setup airflow

--- Setting up the database servers/connection_strings.sh
+++ Setting up the database servers/connection_strings.sh
@@ -5,7 +5,7 @@ mysql --protocol=tcp -h localhost -P 3306 -D s_retail_db -u s_retail_user -p
 psql -h localhost -p 5432 -d t_retail_db -U t_retail_user -W 
 
 -- Port forwarding
-ssh -i ~/.ssh/google_compute_engine -fNL 3306:localhost:3306 -fNL 5432:localhost:5432  user@IP
+ssh -i ~/.ssh/google_compute_engine -fNL 3306:localhost:3306 -fNL 5432:localhost:5432 8080:localhost:8080  user@IP
 ps aux | grep ssh
 
 # in case of errors when connecting to DB from python with mysql connector, use
